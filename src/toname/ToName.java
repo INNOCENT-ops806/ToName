f6a7b4c12ee81f76e5477989bc2d8426414dc415
hexaredecimal <mfanakagama@gmail.com>
2025-04-05 14:25:25 +0200

    src: Added the backend code

--- src/toname/ToName.java
+++ src/toname/ToName.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package toname;
+
+import blazing.Blazing;
 
 /**
  *
@@ -14,7 +12,6 @@ public class ToName {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+			Blazing.run(BackServer.class);
+		}
 }
